dbd647d1b4ac084ae570e7f4a2ed7ca8f5fae020
SeongJJun <tjdwns@gmail.com>
2025-11-18 13:48:09 +0900

    configserver checkstyle 적용.

--- src/main/java/com/athenhub/configserver/ConfigServerApplication.java
+++ src/main/java/com/athenhub/configserver/ConfigServerApplication.java
@@ -4,12 +4,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * Spring Boot 애플리케이션의 진입점 클래스.
+ *
+ * <p>이 클래스는 {@link SpringBootApplication}을 사용하여 Spring Boot 환경에서 애플리케이션을 실행합니다.
+ *
+ * @author 김형섭
+ * @since 1.0.0
+ */
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ConfigServerApplication.class, args);
-    }
-
+  /**
+   * 애플리케이션 실행 진입점.
+   *
+   * @param args 명령줄 인자
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(ConfigServerApplication.class, args);
+  }
 }
